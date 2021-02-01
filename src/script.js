473ee6e56fc90b390fff93f11cc22581eab91ee2
samaranand <samarss2015@gmail.com>
2021-02-01 16:16:07 +0530

    day-1

--- src/script.js
+++ src/script.js
@@ -1,10 +1,13 @@
 let result, currentTime;
 
-let running = document.getElementById("running");
-let upcoming = document.getElementById("upcoming");
-let in24hr = document.getElementById("in-24-hr");
+let running = document.getElementById("running-value");
+let upcoming = document.getElementById("upcoming-value");
+let in24hr = document.getElementById("in24hr-value");
+let value = document.querySelectorAll(".value");
 
 let dark_check = document.getElementById("dark-btn");
+
+// working of dark btn
 
 dark_check.addEventListener("click", () => {
   if (dark_check.value == "on") {
@@ -17,6 +20,10 @@ dark_check.addEventListener("click", () => {
       "text-lighten-2",
       "text-darken-4"
     );
+    value.forEach((e) => {
+      e.classList.remove("darken-3");
+      e.classList.add("lighten-2");
+    });
   } else {
     dark_check.value = "on";
     document.body.className = document.body.className.replace(
@@ -27,6 +34,10 @@ dark_check.addEventListener("click", () => {
       "text-darken-4",
       "text-lighten-2"
     );
+    value.forEach((e) => {
+      e.classList.remove("lighten-2");
+      e.classList.add("darken-3");
+    });
   }
 });
 
@@ -41,20 +52,40 @@ function convertToHour(seconds) {
 }
 
 function returnConverted(r) {
-  const utcDate = r["start_time"];
-  const utcDate2 = r["end_time"];
-  const localDate = new Date(utcDate);
-  const localDate2 = new Date(utcDate2);
-
-  const v = `<div class="value" id="value">
-          <div class="name" id="name">${r["name"]}</div>
-          <div class="link" id="link"><a href=" ${r["url"]}">link</a></div>
-          <div class="duration" id="duration">${convertToHour(
-            r["duration"]
-          )}</div>
-        </div><br/>`;
-  return v;
+  const start_utcDate = r["start_time"];
+  const end_utcDate = r["end_time"];
+  const startDate = new Date(start_utcDate);
+  const endDate = new Date(end_utcDate);
+  // console.log(startDate.toDateString());
+  const t = `<section class="section">
+  <div class="col s12 m12 l12">
+    <span class="platform flow-text left" id="name"
+      ><a href="${r["url"]}">${r["name"]}</a></span
+    >
+  </div>
+  <div class="col s12 m6 l6">
+    <span class="platform flow-text left" id="platform"
+      >${r["site"]}</span
+    >
+  </div>
+  <div class="col s12 m6 l6" id="duration">
+    <span class="platform flow-text left" id="duration"
+      >${convertToHour(r["duration"])}</span
+    >
+  </div>
+  <div class="col s6 m6 l6">
+    <span><b>From - ${startDate.toDateString()}</b></span>
+  </div>
+  <div class="col s6 m6 l6">
+    <span><b>To - ${endDate.toDateString()}</b></span>
+  </div>
+</section> <div class="col s12 m12 l12"><hr /></div>`;
+  return t;
 }
+
+let running_count = 0,
+  upcomin_count = 0,
+  in24hr_count = 0;
 
 let updateUI = function () {
   fetch("http://worldclockapi.com/api/json/utc/now")
@@ -65,16 +96,26 @@ let updateUI = function () {
     .then(() => {
       result.forEach((r) => {
         // console.log(r['name']);
+
         if (r["start_time"] <= currentTime) {
           let v = returnConverted(r);
-          running.insertAdjacentHTML("beforeend", v);
+          if (running_count < 10) {
+            running.insertAdjacentHTML("beforeend", v);
+            running_count += 1;
+          }
         } else {
           let v = returnConverted(r);
-          upcoming.insertAdjacentHTML("beforeend", v);
+          if (upcomin_count < 10) {
+            upcoming.insertAdjacentHTML("beforeend", v);
+            upcomin_count += 1;
+          }
         }
         if (r["in_24_hours"] === "Yes") {
           let v = returnConverted(r);
-          in24hr.insertAdjacentHTML("beforeend", v);
+          if (in24hr_count < 10) {
+            in24hr.insertAdjacentHTML("beforeend", v);
+            in24hr_count += 1;
+          }
         }
       });
     });
@@ -90,6 +131,12 @@ let initFun = function () {
     });
 };
 
-// initFun();
+initFun();
 
+// materialize-css-js
 
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".scrollspy");
+  var instances = M.ScrollSpy.init(elems);
+  console.log("samar");
+});
